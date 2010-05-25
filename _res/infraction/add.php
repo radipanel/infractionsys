54ab69001462e2e1136a777055645b2299afdbf0
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-25 23:50:43 +0100

    Addition of field checking for first run addition of fields and tables

--- _res/infraction/add.php
+++ _res/infraction/add.php
@@ -2,6 +2,25 @@
 if( !preg_match( "/index.php/i", $_SERVER['PHP_SELF'] ) ) { die(); }
 
 require_once( "functions.php" );
+
+$num_infract_exist = checkdbfield("num_infract");
+$num_warn_exist = checkdbfield("num_warn");
+
+switch $num_infract_exist
+	case true:
+		break;
+
+	case false:
+		$db->query( "ALTER TABLE `users` ADD `num_infract` INT(1) NOT NULL DEFAULT ‘0′" );
+		break;
+
+switch $num_warn_exist
+	case true:
+		break;
+
+	case false:
+		$db->query( "ALTER TABLE `users` ADD `num_warn` INT(1) NOT NULL DEFAULT ‘0′" );
+		break;
 ?>
 <form action="" method="post" id="addInfraction">
 
