ea046bcd1bb130f516535f28860c002a1d2e67ef
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-26 00:23:49 +0100

    Added extra functions for adding warning and infraction to a user

--- _res/infraction/functions.php
+++ _res/infraction/functions.php
@@ -28,4 +28,20 @@ function checkdbfield($fieldname) {
 
 	}
 }
+
+/*
+ * addinfractiontouser - adds an infraction to a user.
+ * @param  $username - required. the name of the user we are infracting.
+ * @return $q      - true if the warning was added, false if it wasn't.
+ */
+function addinfractiontouser($username) {
+}
+
+/*
+ * addwarningtouser - adds an warning to a user.
+ * @param  $username - required. the name of the user we are warning.
+ * @return $q      - true if the warning was added, false if it wasn't.
+ */
+function addwarningtouser($username) {
+}
 ?>
