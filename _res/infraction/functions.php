b88a68a83229220528b2e86104a460c98b321b17
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-26 17:45:24 +0100

    Minor readability changes, including changing case of functions

--- _res/infraction/functions.php
+++ _res/infraction/functions.php
@@ -2,38 +2,38 @@
 require_once( "_inc/glob.php" );
 
 /*
- * addinfractiontouser - adds an infraction to a user.
+ * addInfractionToUser - adds an infraction to a user.
  * @param  $username - required. the name of the user we are infracting.
  * @return $q      - true if the warning was added, false if it wasn't.
  */
-function addinfractiontouser($username) {
+function addInfractionToUser( $username ) {
 
 }
 
 /*
- * removeinfractionfromuser - removes an infraction from a user.
+ * removeInfractionFromUser - removes an infraction from a user.
  * @param  $username - required. the name of the user we are removing an infraction.
  * @return $q      - true if the infraction was removed, false if it wasn't.
  */
-function removeinfractionfromuser($username) {
+function removeInfractionFromUser( $username ) {
 
 }
 
 /*
- * addwarningtouser - adds an warning to a user.
+ * addWarningToUser - adds an warning to a user.
  * @param  $username - required. the name of the user we are warning.
  * @return $q      - true if the warning was added, false if it wasn't.
  */
-function addwarningtouser($username) {
+function addWarningToUser( $username ) {
 
 }
 
 /*
- * removewarningfromuser - removes a warning from a user.
+ * removeWarningFromUser - removes a warning from a user.
  * @param  $username - required. the name of the user we are removing a warning.
  * @return $q      - true if the warning was removed, false if it wasn't.
  */
-function removewarningfromuser($username) {
+function removeWarningFromUser( $username ) {
 
 }
 ?>
