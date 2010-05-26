187e7fa8656ee93355bece8e5be5338c0374f692
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-26 17:43:16 +0100

    Added planned extra functions to functions.php

--- _res/infraction/functions.php
+++ _res/infraction/functions.php
@@ -11,11 +11,29 @@ function addinfractiontouser($username) {
 }
 
 /*
+ * removeinfractionfromuser - removes an infraction from a user.
+ * @param  $username - required. the name of the user we are removing an infraction.
+ * @return $q      - true if the infraction was removed, false if it wasn't.
+ */
+function removeinfractionfromuser($username) {
+
+}
+
+/*
  * addwarningtouser - adds an warning to a user.
  * @param  $username - required. the name of the user we are warning.
  * @return $q      - true if the warning was added, false if it wasn't.
  */
 function addwarningtouser($username) {
+
+}
+
+/*
+ * removewarningfromuser - removes a warning from a user.
+ * @param  $username - required. the name of the user we are removing a warning.
+ * @return $q      - true if the warning was removed, false if it wasn't.
+ */
+function removewarningfromuser($username) {
 
 }
 ?>
