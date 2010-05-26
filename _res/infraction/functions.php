8a840ab0b793c4dbc8481c4afcf40d7b1e365e6b
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-26 17:36:46 +0100

    Removed checkdbfield() function from functions.php to reflect changes in add.php

--- _res/infraction/functions.php
+++ _res/infraction/functions.php
@@ -2,34 +2,6 @@
 require_once( "_inc/glob.php" );
 
 /*
- * checkdbfield - checks MySQL table for field existance.
- * @param  $fieldname - required. the name of the field we are trying to find.
- * @return $q      - true if the field exists, false if it doesn't.
- */
-function checkdbfield($fieldname) {
-	
-	$fields = $db->query( "SHOW COLUMNS FROM users [LIKE '". $fieldname ."']" );
-	$columns = mysql_num_fields($fields);
-	
-	for ($i = 0; $i < $columns; $i++) {
-
-		$field_array[] = mysql_field_name($fields, $i);
-
-	}
-
-	if (!in_array($fieldname, $field_array)) {
-
-		return true;
-
-	}
-	else {
-
-		return false;
-
-	}
-}
-
-/*
  * addinfractiontouser - adds an infraction to a user.
  * @param  $username - required. the name of the user we are infracting.
  * @return $q      - true if the warning was added, false if it wasn't.
