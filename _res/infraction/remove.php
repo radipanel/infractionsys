692272c62d1b2062394b8784756b1de4104959b1
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-30 17:43:06 +0100

    Modified remove.php

--- _res/infraction/remove.php
+++ _res/infraction/remove.php
@@ -14,11 +14,11 @@ if ( $_POST['submitted'] == "true" ) {
 	// So, they've submitted the form, so we clean up some of the inputs
 	$reason = $db->clean( $_POST['reason'] );
 	
-	// And then we figure out if they are issuing an infraction or a warning
+	// And then we figure out if they are removing an infraction or a warning
 	if ( $_POST['type'] == "infraction" ) {
 		
-		// They are issuing an infraction
-		$infraction_issue = addInfractionToUser( $username, $reason );
+		// They are removing an infraction
+		$infraction_issue = removeInfractionFromUser( $username, $reason );
 
 		// Check if the process finished successfully
 		switch ( $infraction_issue ) {
@@ -38,10 +38,10 @@ if ( $_POST['submitted'] == "true" ) {
 	else if ( $_POST['type'] == "warning" ) {
 
 		// They are issuing an infraction
-		$warning_issue = addWarningToUser( $username, $reason );
+		$warning_remove = removeWarningFromUser( $username, $reason );
 
 		// Check if the process finished successfully
-		switch ( $warning_issue ) {
+		switch ( $warning_remove ) {
 			
 			case true:
 				// It did
@@ -62,10 +62,10 @@ else {
 	<div class="box">
 
 		<div class="square title">
-			<strong>Issue Infraction / Warning To User</strong>
+			<strong>Remove Infraction / Warning From User</strong>
 		</div>
 
-		<p>Naughty users? Give them an infraction! Select a user, and click add!</p>
+		<p>Gace an infraction by mistake? Remove their infraction or warning here! Select a user, and click remove!</p>
 		<label for="username">User:</label>
 		<select name="username" id="username">
 		<?php
@@ -93,7 +93,7 @@ else {
 
 		<input type="hidden" name="submitted" value="true" />
 
-		<input type="submit" value="Add!" />
+		<input type="submit" value="Remove!" />
 </form>
 <?php
 }
