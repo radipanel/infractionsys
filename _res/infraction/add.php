b014a2b186a6c6e09dfcb4fa6069ae2df7c8d6e8
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-30 16:51:06 +0100

    Committed revised add.php

--- _res/infraction/add.php
+++ _res/infraction/add.php
@@ -1,8 +1,61 @@
 <?php
+// Prevent the script from being called directly
 if( !preg_match( "/index.php/i", $_SERVER['PHP_SELF'] ) ) { die(); }
 
+// Require the functions.php file
 require_once( "functions.php" );
 
+// Check if the form has been submitted
+if ( $_POST['submitted'] == "true" ) {
+
+	// Insert data into variables
+	$username = $_POST['username'];
+
+	// So, they've submitted the form, so we clean up some of the inputs
+	$reason = $db->clean( $_POST['reason'] );
+	
+	// And then we figure out if they are issuing an infraction or a warning
+	if ( $_POST['type'] == "infraction" ) {
+		
+		// They are issuing an infraction
+		$infraction_issue = addInfractionToUser( $username, $reason );
+
+		// Check if the process finished successfully
+		switch ( $infraction_issue ) {
+			
+			case true:
+				// It did
+				$status = true;
+				break;
+			
+			case false:
+				// It failed :(
+				$status = false;
+				break;		
+		}
+
+	}
+	else if ( $_POST['type'] == "warning" ) {
+
+		// They are issuing an infraction
+		$warning_issue = addWarningToUser( $username, $reason );
+
+		// Check if the process finished successfully
+		switch ( $warning_issue ) {
+			
+			case true:
+				// It did
+				$status = true;
+				break;
+			
+			case false:
+				// It failed :(
+				$status = false;
+				break;		
+		}
+	}
+}
+else {
 ?>
 <form action="" method="post" id="addInfraction">
 
@@ -13,8 +66,8 @@ require_once( "functions.php" );
 		</div>
 
 		<p>Naughty users? Give them an infraction! Select a user, and click add!</p>
-		<label for="user">User:</label>
-		<select name="user" id="user">
+		<label for="username">User:</label>
+		<select name="username" id="user">
 		<?php
 			// First, we grab all the users
 			$getUsers = $db->query( "SELECT * FROM users" );
@@ -30,6 +83,11 @@ require_once( "functions.php" );
 		<label for="reason">Reason:</label>
 		<input type="text" name="reason" />
 
+		<input type="hidden" name="submitted" value="true" />
+
 		<input type="submit" value="Add!" />
 </form>
+<?php
+}
+?>
 
