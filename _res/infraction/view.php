3e2e88a2e5b08df4db7f634aa0c0c4a6aeed32e5
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-31 00:10:14 +0100

    Revised view.php to reflect alpha build status. It's alpha, so not on production systems!

--- _res/infraction/view.php
+++ _res/infraction/view.php
@@ -12,4 +12,6 @@ require_once( "functions.php" );
 		<div class="square title">
 			<strong>View Infraction Log For User</strong>
 		</div>
+
+		<p>This feature hasn't been coded yet.... But remember that this version is considered <strong>alpha</strong> code and not safe for a production site. I'll get around to it, I promise</p>
 </form>
