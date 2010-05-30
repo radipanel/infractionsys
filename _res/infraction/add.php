6fed81899f8cd7d5b1b21825764a467f5beece4c
Matthew Gall <matthew@matthew-ubuntu.(none)>
2010-05-30 15:30:10 +0100

    Updated add.php to incorporate user selection

--- _res/infraction/add.php
+++ _res/infraction/add.php
@@ -12,5 +12,20 @@ require_once( "functions.php" );
 			<strong>Issue Infraction / Warning To User</strong>
 		</div>
 
+		<p>Naughty users? Give them an infraction! Select a user, and click add!</p>
+		<label for="user">User:</label>
+		<select name="user" id="user">
+		<?php
+			// First, we grab all the users
+			$getUsers = $db->query( "SELECT * FROM users" );
+			// And then we use a while loop to create a select HTML statement
+			while( $array = $db->assoc( $getUsers ) ) {
+		?>
+		<option value="<?php echo $array['username']; ?>"><?php echo $array['username']; ?></option>
+		<?php
+			}
+		?>
+		</select>
+
 </form>
 
